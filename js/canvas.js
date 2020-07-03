30c8788123e50b46c054d9fdbe8b2d85b21bd250
Riya Raj <riyaraj0409@gmail.com>
2020-07-04 03:34:22 +0530

    active button: R

--- js/canvas.js
+++ js/canvas.js
@@ -9,7 +9,7 @@
     const SPACE_SIZE = 150;
 
 function drawBoard(){
-    
+    /*
     let id = 0
     for(let i = 0; i < ROW; i++){
         board[i] = [];
@@ -21,7 +21,12 @@ function drawBoard(){
             ctx.strokeStyle = "white";
             ctx.strokeRect(j * SPACE_SIZE, i * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
         }
-    }
+    }*/
+    ctx.lineWidth = 5;
+    ctx.strokeRect(150, 0, 0,450);
+    ctx.strokeRect(300, 0, 0,450);
+    ctx.strokeRect(0, 150, 450, 0);
+    ctx.strokeRect(0, 300, 450, 0);
 }
 drawBoard();
 
--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
+const doc = document.getElementById("robot");
+const hum = document.getElementById("human");
 
+doc.addEventListener("click", function() {
+    doc.classList.add("selected");
+    hum.classList.remove("selected");
+});
 
+hum.addEventListener("click", function() {
+    hum.classList.add("selected");
+    doc.classList.remove("selected");
+});
+
+function changeLevel() {
+    for(let j = 1; j<=5; j++) {
+        document.getElementById(j).addEventListener("click", function(){
+            for(let i = 1; i <= 5 ; i++) {
+                document.getElementById(i).classList.remove("selected");
+            }            
+            document.getElementById(j).classList.add("selected");
+        })
+    }
+};
+
+changeLevel();
